2325df1ee7a0d1c13469d0dc1fa3d09c13cc7f50
Mojicator <oscar2mojica@gmail.com>
2019-11-05 02:23:59 -0600

    Public Page
    This adds a main page for the server.
    
    - Add the index.html
    - Add a command to compile typecript and copy the public folder to dist
    directory

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 
 export default class Server {
     public app: express.Application;
@@ -13,8 +14,15 @@ export default class Server {
         return new Server(port)
     }
 
+    private publicFolder() {
+        const publicPath = path.resolve(__dirname, "../public");
+
+        this.app.use(express.static(publicPath));
+    }
+
     start(callback: () => void) {
         this.app.listen(this.port, callback);
+        this.publicFolder();
     }
 
 }
