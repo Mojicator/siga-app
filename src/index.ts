e3324ea6cafc495d7abe0a034b92c1a790a5455a
Mojicator <oscar2mojica@gmail.com>
2019-11-06 00:56:25 -0600

    Connecting to the database
    Establish a connection to the database using a defined URL.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import Server from './server/server';
+import MongoDB from './database/mongodb';
 
+const database = MongoDB.init("mongodb://localhost:27017/siga-development");
 const server = Server.init(3000);
 
+database.mongoSetup((err: any) => {
+    if (err) throw err;
+    console.log("Database online");
+});
 
 server.start( () => {
-    console.log("Servidor corriendo");
+    console.log("Server running");
 });
