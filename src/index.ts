1ea5243374b139f57335fe69b66e654612814ad6
Mojicator <oscar2mojica@gmail.com>
2019-11-07 04:22:41 -0600

    Environment Variables
    The environment variables could show sensible
    information about how the application works.
    - Add a file to manage the environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import "./config/env"
 import Server from './server/server';
 import MongoDB from './database/mongodb';
 
-const database = MongoDB.init("mongodb://localhost:27017/siga-development");
-const server = Server.init(3000);
+const database = MongoDB.init(process.env.URL as string);
+const server = Server.init(process.env.PORT as unknown as number);
 
 database.mongoSetup((err: any) => {
     if (err) throw err;
