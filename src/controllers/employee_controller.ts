2e24045b000c1db3c5a42df1b53121a95d13eb24
Mojicator <oscar2mojica@gmail.com>
2019-11-08 03:40:22 -0600

    Finishing Employee Model
    This completes the CRUD for employees adding the showById,
    update and delete methods.

--- src/controllers/employee_controller.ts
+++ src/controllers/employee_controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import Employee, { IEmployee } from '../models/employee';
 import bcrypt from 'bcrypt';
+import { ObjectID, ObjectId } from 'bson';
 
 const Rounds: number = 10;
 
@@ -23,8 +24,61 @@ export const allEmployees = (req: Request, res: Response) => {
     });
 }
 
+export const employeeById = (req: Request, res: Response) => {
+    let id: ObjectID = new ObjectId(req.params.id);
+    Employee.findById(id, (err: any, employee: IEmployee) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!employee) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "Employee not found."
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            employee
+        });
+    });
+}
+
+export const updateEmployee = (req: Request, res: Response) => {
+    let id: ObjectID = new ObjectId(req.params.id);
+    let body: IEmployee = req.body;
+    Employee.findByIdAndUpdate(id, body, { new: true }, (err: any, employee: IEmployee | null) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!employee) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "Employee not found."
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            employee
+        });
+    });
+}
+
 export const addEmployee = (req: Request, res: Response) => {
-    let body = req.body;
+    let body: IEmployee = req.body;
     let employee = new Employee({
         firstName: body.firstName,
         lastName: body.lastName,
@@ -45,6 +99,32 @@ export const addEmployee = (req: Request, res: Response) => {
         res.json({
             ok: true,
             employee: employeeDB
+        });
+    });
+}
+
+export const deleteEmployee = (req: Request, res: Response) => {
+    let id: ObjectID = new ObjectId(req.params.id);
+    Employee.findByIdAndRemove(id, (err: any, employee: IEmployee | null) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!employee) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "Employee not found."
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            employee
         });
     });
 }
--- src/routes/employee_router.ts
+++ src/routes/employee_router.ts
@@ -4,6 +4,9 @@ import * as EmployeeController from '../controllers/employee_controller';
 const router: Router = Router();
 
 router.get("/employees", EmployeeController.allEmployees);
+router.get("/employee/:id", EmployeeController.employeeById);
 router.post("/employee", EmployeeController.addEmployee);
+router.put("/employee/:id", EmployeeController.updateEmployee);
+router.delete("/employee/:id", EmployeeController.deleteEmployee);
 
 export default router;
