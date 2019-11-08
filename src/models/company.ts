2393523c5f3a219b3e2a2d31014ff07ed52e2437
Mojicator <oscar2mojica@gmail.com>
2019-11-07 23:30:58 -0600

    Company Model
    This adds an array to handle the employees from a company.

--- src/models/company.ts
+++ src/models/company.ts
@@ -4,12 +4,14 @@ export interface ICompany extends Document {
     name: string;
     giro: string;
     key: string;
+    employees?: Schema.Types.ObjectId[];
 }
 
 const CompanySchema: Schema = new Schema({
     name: { type: String, required: true, unique: true },
     giro: { type: String, required: true },
-    key: { type: String, required: true }
+    key: { type: String, required: true },
+    employees: [{ type: Schema.Types.ObjectId, ref: "Employee" }]
 });
 
 export default mongoose.model<ICompany>("Company", CompanySchema);
